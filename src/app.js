15f981ff2b1ba74cc3d899c877238fa512258384
Marshall Polaris <marshall@pol.rs>
2019-09-06 17:06:50 -0700

    Don't use /api prefix anymore

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const api = require('./api');
 const app = express();
 const logger = morgan(process.env.REQ_LOG_FORMAT, { stream: { write: msg => debug(msg.trimEnd()) } });
 app.use(logger);
-app.use('/api', api.create());
+app.use('/', api.create());
 app.use(function (req, res, _next) {
   res.status(404).send({ error: "No such endpoint." });
 });
