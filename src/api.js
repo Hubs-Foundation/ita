d64cf2db6d294d575da3f91510c7307d979c4de5
Marshall Polaris <marshall@pol.rs>
2019-09-06 17:05:52 -0700

    Establish better retry policy for PS

--- src/api.js
+++ src/api.js
@@ -17,8 +17,17 @@ function create() {
     () => new AWS.EnvironmentCredentials('AWS'),
     () => new AWS.SharedIniFileCredentials()
   ]);
-  const cloudFormation = new CloudFormation({ credentialProvider, region: process.env.AWS_REGION });
-  const parameterStore = new ParameterStore({ credentialProvider, region: process.env.AWS_REGION });
+  const cloudFormation = new CloudFormation({
+    credentialProvider,
+    region: process.env.AWS_REGION,
+    // logger: { write: msg => debug(msg.trimEnd()) }
+  });
+  const parameterStore = new ParameterStore({
+    credentialProvider,
+    region: process.env.AWS_REGION,
+    retryDelayOptions: { base: 5000 }, // very conservative retry rate because of free tier rate limiting
+    // logger: { write: msg => debug(msg.trimEnd()) }
+  });
   const habitat = new Habitat(process.env.HAB_HTTP_HOST, process.env.HAB_HTTP_PORT,
                               process.env.HAB_SUP_HOST, process.env.HAB_SUP_PORT);
   const schemas = loadSchemas(path.join(__dirname, "..", "schemas"));
