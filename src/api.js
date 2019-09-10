cc61a4acf6532d30513ab7cf346eb2a0adf7cc10
Marshall Polaris <marshall@pol.rs>
2019-09-09 17:33:38 -0700

    Implement config /update endpoint

--- src/api.js
+++ src/api.js
@@ -11,6 +11,10 @@ function forwardExceptions(routeFn) {
   return (req, res, next) => routeFn(req, res).catch(next);
 }
 
+function getVersion(ts) {
+  return Math.floor(ts / 1000);
+}
+
 function create() {
 
   // accept credentials from either ~/.aws/credentials file, or from standard AWS_ env variables
@@ -39,6 +43,25 @@ function create() {
 
   const router = express.Router();
 
+  async function flushDiffs(service, now) {
+    debug(`Flushing service ${service}...`);
+    const newConfigs = await parameterStore.read(service);
+    const oldConfigs = await habitat.read(service, process.env.HAB_SERVICE_GROUP);
+    const differences = diff(oldConfigs, newConfigs);
+    if (differences != null) {
+      const diffPaths = new Set();
+      for (const d of differences) {
+        diffPaths.add(d.path.join("/"));
+      }
+      debug(`Updating Habitat configs: ${Array.prototype.join(diffPaths, ', ')}`);
+      await habitat.write(service, process.env.HAB_SERVICE_GROUP, newConfigs, getVersion(now));
+      return diffPaths;
+    } else {
+      debug(`All ${service} configs already up-to-date.`);
+      return null;
+    }
+  }
+
   router.get('/schemas', forwardExceptions(async (req, res) => {
     res.json(schemas);
   }));
@@ -53,13 +76,13 @@ function create() {
     }
     const stackOutputs = await cloudFormation.read(process.env.AWS_STACK_ID);
     const services = req.params.service ? [req.params.service] : Object.keys(schemas);
+    const now = Date.now();
     for (const srv of services) {
       debug(`Initializing service ${srv}...`);
       const schema = schemas[srv];
       const defaults = getDefaults(schema, stackOutputs);
       await parameterStore.write(srv, defaults);
-      const version = Math.floor(Date.now() / 1000);
-      await habitat.write(srv, process.env.HAB_SERVICE_GROUP, defaults, version);
+      await habitat.write(srv, process.env.HAB_SERVICE_GROUP, defaults, getVersion(now));
     }
     return res.json({ msg: `Initialization done. Services up-to-date: ${services.join(", ")}` });
   }));
@@ -70,24 +93,25 @@ function create() {
       return res.status(400).json({ error: "Invalid service name." });
     }
     const services = req.params.service ? [req.params.service] : Object.keys(schemas);
+    const now = Date.now();
     for (const srv of services) {
-      debug(`Flushing service ${srv}...`);
-      const newConfigs = await parameterStore.read(srv);
-      const oldConfigs = await habitat.read(srv, process.env.HAB_SERVICE_GROUP);
-      const differences = diff(oldConfigs, newConfigs);
-      if (differences != null) {
-        const diffPaths = new Set();
-        for (const d of differences) {
-          diffPaths.add(d.path.join("/"));
-        }
-        const version = Math.floor(Date.now() / 1000);
-        await habitat.write(srv, process.env.HAB_SERVICE_GROUP, newConfigs, version);
-        return res.json({ msg: `Flush done. Services up-to-date: ${services.join(", ")}` });
-      } else {
-        return res.json({ msg: `Flush done. No service needed updates.` });
-      }
+      await flushDiffs(srv, now);
     }
+    return res.json({ msg: `Flush done. Services up-to-date: ${services.join(", ")}` });
   }));
+
+  // updates parameter store with new client-supplied values and flushes them to ring
+  router.post('/update/:service', forwardExceptions(async (req, res) => {
+    if (!(req.params.service in schemas)) {
+      return res.status(400).json({ error: "Invalid service name." });
+    }
+    debug(`Updating ${req.params.service} with new values.`);
+    // todo: validate against schema?
+    await parameterStore.write(req.params.service, req.body);
+    await flushDiffs(req.params.service, Date.now());
+    return res.json({ msg: `Update succeeded.` });
+  }));
+
 
   return router;
 }
--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-const express = require('express');
 const debug = require('debug')('ita');
+const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const api = require('./api');
 
 const app = express();
 const logger = morgan(process.env.REQ_LOG_FORMAT, { stream: { write: msg => debug(msg.trimEnd()) } });
 app.use(logger);
+app.use(bodyParser.json({ strict: true }));
 app.use('/', api.create());
 app.use(function (req, res, _next) {
   res.status(404).send({ error: "No such endpoint." });
