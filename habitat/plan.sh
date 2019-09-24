bc76dece979645c6c29b236d37b4b348745c15eb
Marshall Polaris <marshall@pol.rs>
2019-09-24 15:16:56 -0700

    Fix up, simplify Habitat plan

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -13,12 +13,19 @@ pkg_build_deps=(
   core/git
 )
 
+do_prepare() {
+  # we need /usr/bin/env for webpack CLI
+  [[ ! -f /usr/bin/env ]] && ln -s "$(pkg_path_for coreutils)/bin/env" /usr/bin/env
+  return 0;
+}
+
 do_build() {
-  npm ci
+  npm ci && npm run build
 }
 
 do_install() {
-  for p in node_modules package.json package-lock.json bin src schemas
+  # take care not to include unminified ita source
+  for p in node_modules dist schemas
   do
     cp -R ./$p "$pkg_prefix"
   done
