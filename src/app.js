27e1f0f3e58150d4a41caaf2a7f487d5d8bc2a96
Greg Fodor <gfodor@gmail.com>
2019-10-03 16:18:57 +0000

    Add postgrest, and ready file for startup

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const process = require('process');
 const debug = require('debug')('ita');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -12,6 +14,11 @@ const flush = require("./flush");
 const AUTO_FLUSH_DURATION_MS = 30000;
 
 async function createApp() {
+  // Remove existing ready file, if exists
+  if (process.env.READY_FILE && fs.existsSync(process.env.READY_FILE)) {
+    fs.unlinkSync(process.env.READY_FILE);
+  }
+
   // accept credentials from either ~/.aws/credentials file, or from standard AWS_ env variables
   const credentialProvider = new AWS.CredentialProviderChain([
     () => new AWS.EnvironmentCredentials('AWS'),
@@ -77,6 +84,19 @@ async function createApp() {
   await flushAllServices();
   // Flush all services regularly
   setInterval(flushAllServices, AUTO_FLUSH_DURATION_MS);
+
+  // Touch ready file
+  if (process.env.READY_FILE) {
+    fs.closeSync(fs.openSync(process.env.READY_FILE, 'w'));
+
+    process.on('SIGINT', () => {
+      if (fs.existsSync(process.env.READY_FILE)) {
+        fs.unlinkSync(process.env.READY_FILE);
+      }
+
+      process.exit(); // eslint-disable-line no-process-exit
+    });
+  }
 
   return app;
 }
