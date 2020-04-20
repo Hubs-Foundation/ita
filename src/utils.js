02c85241c049f0f0bee26891826473af444ecd2a
Greg Fodor <gfodor@gmail.com>
2020-04-20 18:25:46 +0000

    Revert "Add generic secret injection xforms"
    This reverts commit 6ab30fbaf9687c75d4e4c1d38e5853bdeeb12f46.

--- src/utils.js
+++ src/utils.js
@@ -9,31 +9,23 @@ function getTimeString() {
 }
 
 async function _performOutputXform(value, xform, args, keymasterSecrets, getSecretValue, getS3Object) {
-  if (xform === "read-aws-secret" || xform === "inject-aws-secret" || xform === "read-keymaster-secret" || xform === "inject-keymaster-secret" || xform == "inject-secrets" || xform == "read-secrets") {
-    let isInjection = xform.startsWith("inject-");
-
-    let isKeymasterAndProvider = xform === "inject-secrets" || xform == "read-secrets";
-    let isKeymasterOnly = xform.indexOf("keymaster") >= 0;
-    let isProviderOnly = xform.indexOf("aws") >= 0;
-
+  if (xform === "read-aws-secret" || xform === "inject-aws-secret" || xform === "read-keymaster-secret" || xform === "inject-keymaster-secret") {
     let secretId = value;
 
-    if (isInjection) {
+    if (xform.startsWith("inject-")) {
       secretId = value.match(/{(.*)}/)[1];
     }
 
     let secretValue;
 
-    if (isKeymasterOnly || isKeymasterAndProvider) {
+    if (xform.indexOf("keymaster") >= 0) {
       secretValue = keymasterSecrets[secretId];
-    }
-
-    if (isProviderOnly || (isKeymasterAndProvider && !secretValue)) {
+    } else {
       const secret = await getSecretValue({ SecretId: secretId });
       secretValue = JSON.parse(secret.SecretString).password; // By convention we just use the key 'password' in stacks
     }
 
-    if (isInjection) {
+    if (xform.startsWith("inject-")) {
       return value.split(`{${secretId}}`).join(secretValue);
     } else {
       return secretValue;
